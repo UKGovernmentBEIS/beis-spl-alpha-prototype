83f443aaf72fee15b09c1303d561e4ab11b0e4d9
Jamie Humphries <jamie.humphries@softwire.com>
2019-03-11 14:25:24 +0000

    #36 Clickable leave calendar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,17 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
+const SHARED = 'shared'
+const NOT_SHARED = 'not-shared'
+
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  $('td.mother:not(.compulsory-maternity), td.partner').click(function () {
+    $this = $(this)
+    const selectedLeaveType = $('input[name=leave-type]:checked').val()
+    const leaveClassesToRemove = [SHARED, NOT_SHARED].filter(className => className != selectedLeaveType)
+    leaveClassesToRemove.forEach(leaveClass => $this.removeClass(leaveClass))
+    $this.toggleClass(selectedLeaveType)
+  })
 })
