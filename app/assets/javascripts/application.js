a085b461f012c63f07b24b92798242b9a91e6444
Jamie Humphries <jamie.humphries@softwire.com>
2019-03-11 16:03:24 +0000

    #36 Add leave weeks summary.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,12 @@ if (window.console && window.console.info) {
 const SHARED = 'shared'
 const NOT_SHARED = 'not-shared'
 
+const TOTAL_WEEKS_LEAVE = 52
+
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  updateWeeksTotals()
 
   $('td.mother:not(.compulsory-maternity), td.partner').click(function () {
     $this = $(this)
@@ -17,5 +21,22 @@ $(document).ready(function () {
     const leaveClassesToRemove = [SHARED, NOT_SHARED].filter(className => className != selectedLeaveType)
     leaveClassesToRemove.forEach(leaveClass => $this.removeClass(leaveClass))
     $this.toggleClass(selectedLeaveType)
+    updateWeeksTotals()
   })
 })
+
+function updateWeeksTotals() {
+  const shared = $('td.shared').length
+  const maternity = $('td.mother.not-shared').length + $('td.mother.compulsory-maternity').length
+  const paternity = $('td.partner.not-shared').length
+  const remaining = TOTAL_WEEKS_LEAVE - shared - maternity
+  $('#shared-weeks').text(shared)
+  $('#maternity-weeks').text(maternity)
+  $('#paternity-weeks').text(paternity)
+  $('#remaining-weeks').text(remaining)
+  if (remaining >= 0) {
+    $('.remaining-total').removeClass('negative')
+  } else {
+    $('.remaining-total').addClass('negative')
+  }
+}
