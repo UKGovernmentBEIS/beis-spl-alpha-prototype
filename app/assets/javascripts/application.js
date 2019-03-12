6440440dcbadd805a6521e0ced1d5344d2a9f14a
Jamie Humphries <jamie.humphries@softwire.com>
2019-03-12 10:43:15 +0000

    #38 Include compulsory maternity leave and add warnings.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@ const SHARED = 'shared'
 const NOT_SHARED = 'not-shared'
 const ALL_LEAVE_TYPES = [SHARED, NOT_SHARED]
 
-const TOTAL_WEEKS_LEAVE = 52
+const MATERNITY_WEEKS_ENTITLEMENT = 52
+const PATERNITY_WEEKS_ENTITLEMENT = 2
 
 let isMouseDown = false
 let toggleAction = null
@@ -17,9 +18,9 @@ let toggleAction = null
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 
-  updateWeeksTotals()
+  onLeaveUpdated()
 
-  $('td.mother, td.partner').mousedown(function (e) {
+  $('td.mother:not(.compulsory-maternity), td.partner').mousedown(function (e) {
     // Only respond to primary mouse button.
     if (e.which === 1) {
       const $this = $(this)
@@ -53,27 +54,65 @@ function getToggleAction($cell) {
 
 function removeLeave(leaveType, $cell) {
   $cell.removeClass(leaveType)
-  updateWeeksTotals()
+  onLeaveUpdated()
 }
 
 function setLeave(leaveType, $cell) {
   ALL_LEAVE_TYPES.forEach(type => removeLeave(type, $cell))
   $cell.addClass(leaveType)
-  updateWeeksTotals()
+  onLeaveUpdated()
 }
 
-function updateWeeksTotals() {
-  const shared = $('td.shared').length
-  const maternity = $('td.mother.not-shared').length + $('td.mother.compulsory-maternity').length
-  const paternity = $('td.partner.not-shared').length
-  const remaining = TOTAL_WEEKS_LEAVE - shared - maternity
-  $('#shared-weeks').text(shared)
-  $('#maternity-weeks').text(maternity)
-  $('#paternity-weeks').text(paternity)
-  $('#remaining-weeks').text(remaining)
-  if (remaining >= 0) {
+function onLeaveUpdated() {
+  const motherShared = $('td.mother.shared')
+  const partnerShared = $('td.partner.shared')
+  const sharedWeeks = motherShared.length + partnerShared.length
+  const maternity = $('td.mother.not-shared')
+  const maternityWeeks = maternity.length
+  const paternity = $('td.partner.not-shared')
+  const paternityWeeks = paternity.length
+  const remainingWeeks = MATERNITY_WEEKS_ENTITLEMENT - maternityWeeks - sharedWeeks
+
+  $('#shared-weeks').text(sharedWeeks)
+  $('#maternity-weeks').text(maternityWeeks)
+  $('#paternity-weeks').text(paternityWeeks)
+  $('#remaining-weeks').text(remainingWeeks)
+  if (remainingWeeks >= 0) {
     $('.remaining-total').removeClass('negative')
   } else {
     $('.remaining-total').addClass('negative')
+  }
+
+  const hasMaternityGap = hasGap(maternity)
+  const hasSharedGap = hasGap(motherShared) || hasGap(partnerShared)
+  const hasPaternityGap = hasGap(paternity)
+
+  const paternityOutOfRange = paternity.filter(function () {
+    const week = parseInt($(this).data('week'))
+    return week < 0 || week > 7
+  }).length !== 0
+
+  setVisibility('maternity-gap', hasMaternityGap)
+  setVisibility('maternity-maximum', remainingWeeks < 0)
+  setVisibility('no-more-shared', remainingWeeks <= 0)
+  setVisibility('can-break-shared', sharedWeeks > 0 && !hasSharedGap)
+  setVisibility('paternity-gap', hasPaternityGap)
+  setVisibility('paternity-maximum', paternityWeeks > PATERNITY_WEEKS_ENTITLEMENT)
+  setVisibility('paternity-period', paternityOutOfRange)
+}
+
+function hasGap(leaveCells) {
+  const firstWeek = parseInt(leaveCells.first().data('week'))
+  const lastWeek = parseInt(leaveCells.last().data('week'))
+  const totalWeeks = leaveCells.length
+  return lastWeek - firstWeek >= totalWeeks
+}
+
+function setVisibility(elementId, isVisible) {
+  const $element = $(`#${elementId}`)
+  if (isVisible) {
+    $element.show()
+  } else {
+    $element.hide()
   }
 }
