d247d03956da31d2b08b29d614cc0f23056cdfd3
MatthewBurstein <matthew.burstein@softwire.com>
2019-04-03 12:15:54 +0100

    #121 feat: add logic for elligibility form of individual parent

--- app/routes.js
+++ app/routes.js
@@ -87,6 +87,20 @@ router.post('/shared-parental-leave-planner/key-dates', function (req, res) {
   res.redirect('/shared-parental-leave-planner/key-dates')
 })
 
+router.post('/eligibility-tool/results', function (req, res) {
+  const data = req.session.data
+  const eligibilityData = {
+    employmentStatus: data['employment-status'],
+    continuousWork: data['continuous-work'],
+    payThreshold: data['pay-threshold'],
+    partnerWork: data['partner-work'],
+    partnerPay: data['partner-pay']
+  }
+
+  data.eligibility = getEligibility(eligibilityData)
+  res.redirect('results')
+})
+
 function parseSavedDataFromQuery(query) {
   const savedData = query['s']
   if (!savedData) {
@@ -185,6 +199,34 @@ function getWeeklyEarnings(amount, period) {
     default:
       return null
   }
+}
+
+function getEligibility(eligibilityData) {
+  const isYes = field => field === "yes"
+  const isNo = field => field === "no"
+
+  const {
+    employmentStatus,
+    continuousWork,
+    payThreshold,
+    partnerWork,
+    partnerPay
+  } = eligibilityData
+
+  const eligibility = { spl: false, shpp: false}
+
+  if (isNo(partnerWork) || isNo(partnerPay)) { return eligibility }
+
+  if (employmentStatus === "worker") {
+    eligibility.shpp = isYes(continuousWork) && isYes(payThreshold)
+  }
+
+  if (employmentStatus === "employee") {
+    eligibility.shpp = isYes(continuousWork) && isYes(payThreshold)
+    eligibility.spl = isYes(continuousWork)
+  }
+
+  return eligibility
 }
 
 module.exports = router
