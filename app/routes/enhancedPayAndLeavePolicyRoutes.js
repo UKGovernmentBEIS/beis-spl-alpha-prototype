5c852b1fcbbcaff475df5d92fea16ef7603dd2df
MatthewBurstein <matthew.burstein@softwire.com>
2019-04-17 14:02:11 +0100

    #124 feat: add multiple-blocks-of-leave to enhanced pay policy

--- app/routes/enhancedPayAndLeavePolicyRoutes.js
+++ app/routes/enhancedPayAndLeavePolicyRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/type-of-leave', function (req, res) {
-  console.log(req.session.data)
   if (req.session.data['leave-type'] === 'shared') {
     res.redirect('/enhanced-pay-and-leave-policy/multiple-blocks-of-leave')
   } else {
@@ -10,5 +9,8 @@ router.post('/type-of-leave', function (req, res) {
   }
 })
 
+router.post('/multiple-blocks-of-leave', function (req, res) {
+  res.redirect('/enhanced-pay-and-leave-policy/which-weeks-are-paid')
+})
 
 module.exports = router
