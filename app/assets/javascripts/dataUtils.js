c3dc0e22cdea935fa9646a0951d4b73d09632f78
MatthewBurstein <matthew.burstein@softwire.com>
2019-04-17 16:39:30 +0100

    no-ticket refactor: rename mother's partner and primary-adopter's partner to 'partner'

--- app/assets/javascripts/dataUtils.js
+++ app/assets/javascripts/dataUtils.js
@@ -3,11 +3,11 @@
     if (data['birth-or-adoption'] === 'birth') {
       data['primary-leave-type'] = 'maternity'
       data['primary-name'] = 'mother'
-      data['secondary-name'] = 'mother’s partner'
+      data['secondary-name'] = 'partner'
     } else if (data['birth-or-adoption'] === 'adoption') {
       data['primary-leave-type'] = 'adoption'
       data['primary-name'] = 'primary adopter'
-      data['secondary-name'] = 'primary adopter’s partner'
+      data['secondary-name'] = 'partner'
     }
   }
 })(typeof exports === 'undefined' ? this['utils'] = {} : exports)
