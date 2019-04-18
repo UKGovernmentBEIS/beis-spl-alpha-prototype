a9bb154e58c3c42746b5cc0c241f60ad376b7269
MatthewBurstein <matthew.burstein@softwire.com>
2019-04-18 17:29:58 +0100

    160 feat: add steps of notice flow up to due-date

---
+++ app/routes/noticeRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = router
+
+const dataUtils = require('../assets/javascripts/dataUtils')
+
+router.post('/index', function (req, res) {
+  res.redirect('/notice/birth-or-adoption')
+})
+
+router.post('/birth-or-adoption', function(req, res) {
+  dataUtils.setBirthOrAdoptionInformation(req.session.data)
+  res.redirect('/notice/relation-to-child')
+})
+
+router.post('/relation-to-child', function(req, res) {
+  res.redirect('/notice/which-entitlement')
+})
+
+router.post('/which-entitlement', function (req, res) {
+  res.redirect('/notice/parent-information')
+})
+
+router.post('/parent-information', function (req, res) {
+  const { data } = req.session
+  if (data['primary-parent-information-complete']) {
+    res.redirect('/notice/due-date')
+  } else {
+    data['primary-parent-information-complete'] = true
+    res.redirect('/notice/parent-information')
+  }
+})
