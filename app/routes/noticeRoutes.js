6c817ed7af399e1728eb257bd012710a931ea911
MatthewBurstein <matthew.burstein@softwire.com>
2019-04-23 18:10:38 +0100

    feat: add skip parent functionality to notice/parent-information

--- app/routes/noticeRoutes.js
+++ app/routes/noticeRoutes.js
@@ -22,6 +22,16 @@ router.post('/which-entitlement', function (req, res) {
   res.redirect('/notice/parent-information')
 })
 
+router.get("/parent-information/skip-parent", function (req, res) {
+  const { data } = req.session
+  if (data['primary-parent-information-complete']) {
+    res.redirect('/notice/due-date')
+  } else {
+    data['primary-parent-information-complete'] = true
+    res.redirect('/notice/parent-information')
+  }
+})
+
 router.post('/parent-information', function (req, res) {
   const { data } = req.session
   if (data['primary-parent-information-complete']) {
