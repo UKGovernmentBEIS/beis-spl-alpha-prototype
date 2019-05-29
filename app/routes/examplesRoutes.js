8b7d4f6aa06d34d2f65a79a2e5dbd7bfc66761b7
MatthewBurstein <matthew.burstein@softwire.com>
2019-05-29 17:31:14 +0100

    add html for supporting-the-mother-after-birth

--- app/routes/examplesRoutes.js
+++ app/routes/examplesRoutes.js
@@ -5,4 +5,8 @@ router.get('/managing-parenting-and-your-career', function (req, res) {
   res.render('shared-parental-leave-planner/examples/managing-parenting-and-your-career')
 })
 
+router.get('/supporting-the-mother-after-birth', function (req, res) {
+  res.render('shared-parental-leave-planner/examples/supporting-the-mother-after-birth')
+})
+
 module.exports = router
